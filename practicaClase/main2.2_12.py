bd548c6230d6826e062dcd75d8855a47d77c6177
serquio2000 <serquio2000@gmail.com>
2021-12-03 16:16:59 +0100

    Ejercicio listas scd update

--- practicaClase/main2.2_12.py
+++ practicaClase/main2.2_12.py
@@ -3,6 +3,7 @@ def main():
     telefonList = list()
     ageList = list()
     contactList = list()
+    validateList = list()
     x,y=0,0
     contact= False
     values= int(input("introduce cuantos valores quieres registrar: "))
@@ -12,6 +13,11 @@ def main():
     while x < values:
         name,surname,surname2 = input("introduce un nombre:"),input("introduce el primer apellido:"),input("introduce el segundo apellido:")
         tn,age = input("introduce un numero de telefono:"),int(input("introduce tu edad:"))
+        validate= input("introduce si es correcto o no:")
+        if validate[:1] == "y":
+            validate = True
+        else :
+            validate = False
         code= 0
         contact = int(input("introduce si es registro nuevo;1=nuevo, 0=registrado: "))
         while contact >1 and contact <0:
@@ -29,10 +35,11 @@ def main():
         telefonList.append(tn)
         ageList.append(age)
         contactList.append(contact)
+        validateList.append(validate)
         x += 1
-    print("\tcodi " + "\ttelefon " + "\tedat " + "\tcontacte ")
+    print("\tcodi " + "\ttelefon " + "\tedat " + "\tcontacte "+ "\tValidar")
     while y< len(codeList):
-        print("\t",codeList[y], telefonList[y],"\t", ageList[y],"\t", contactList[y])
+        print("\t",codeList[y], telefonList[y],"\t", ageList[y],"\t", contactList[y],"\t\t\t",validateList[y])
         y += 1
 
 if __name__ == "__main__":
